0fd72619ba55c25e20990d8c2d933928085b81b1
Theodore Stabile <stabilet327@strose.edu>
2019-03-29 19:31:39 -0400

    Update characterEncrypt.py

--- stegaModule/characterEncrypt.py
+++ stegaModule/characterEncrypt.py
@@ -1,52 +1,56 @@
 from PIL import Image
 
-#get text and convert it to list of characters
-text="my name is Jeff"
+def encrypt(text,image):
+     #convert text to list of characters
+     characterList=list(text)
+     print(characterList)
+
+     #include key for message in the message itself during encryption
+     #load image 
+     img = Image.open(image)
+     pixels = img.load()
+     print("Image dimensions",img.size)
+
+     pixelBufferX=100 #space between affected columns
+     pixelBufferY=100 # same as pBx, but for rows
+
+     #pixel x/y, start off at 100,100
+     pX=100
+     pY=100
+     timeUntilNextRow=10
+     #iterate every 100th pixel 
+     #store 10 characters on each row, spaced 100 pixel between each
+     for char in characterList:
+          #collect color value of current pixel, cast to list
+          rgbValue = list(pixels[pX,pY])
+     
+          r = rgbValue[0] #original value
+          g = rgbValue[1]
+          b = rgbValue[2]
+     
+          placeHolder=list(pixels[pX-1,pY]) #r value from adjacent pixele
+          placeHolder=placeHolder[0]
+
+          r = placeHolder - ord(char) #new value(adjacent pixel's value - char's ASCII value
+
+          pixels[pX,pY] = (r,g,b)
+          pixelXY='('+str(pX)+','+str(pY)+')'
+          print("Current pixel location (x,y) is",pixelXY)
+          print("RGB value before adding",char,"=",rgbValue) 
+          print("RGB value after adding",char,"=",pixels[pX,pY],'\n')
+     
+          #move to the next pixel in column(AKA x), after 10 column shifts, go to next row(AKA y)
+          pX+=100
+          timeUntilNextRow-=1
+          if timeUntilNextRow==0:
+               pX=100
+               pY+=100
+               timeUntilNextRow=10
+     #after all characters are encrpyted, show image!       
+     img.show()
+     img.save("encryptedImage.png")
+
+text="Jeff"
 print("Text to encrypt:",text)
-characterList=list(text)
-print(characterList)
-
-#load image 
-img = Image.open("apple.jpg")
-pixels = img.load()
-print("Image dimensions",img.size)
-
-pixelBufferX=100 #space between affected columns
-pixelBufferY=100 # same as pBx, but for rows
-#TEST 
-#HOWDY!
-#pixel x/y, start off at 100,100
-pX=100
-pY=100
-px=200-100
-timeUntilNextRow=10
-#iterate every 100th pixel 
-#store 10 characters on each row, spaced 100 pixel between each
-for char in characterList:
-     #collect color value of current pixel, cast to list
-     rgbValue = list(pixels[pX,pY])
-     
-     r=rgbValue[0]
-     g=rgbValue[1]
-     b=rgbValue[2]
-     
-     pixelXY='('+str(pX)+','+str(pY)+')'
-     print("Current pixel location (x,y) is",pixelXY)
-     print("RGB value before adding",char,"=",rgbValue)
-     
-     r=r-ord(char)#change the red value by subtracting the characters ASCII Value
-     pixels[pX,pY] = (r,g,b)
-     
-     print("RGB value after adding",char,"=",pixels[pX,pY],'\n')
-     
-     #move to the next column(x), after 10 column shifts, go to next row(y)
-     pX+=100
-     timeUntilNextRow-=1
-     if timeUntilNextRow==0:
-          pX=100
-          pY+=100
-          timeUntilNextRow=10
-#after all characters are encrpyted, show image!       
-#img.show()
-img.save('encryptedImage.jpg')
+encrypt(text,"apple.jpg")
 
