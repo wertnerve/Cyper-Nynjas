e59cc5589165f683039b3ba9e175af28fed6ab4a
KieransOtherAccount <sullivank161@strose.edu>
2019-04-16 15:25:06 -0400

    Add files via upload
    GUI updated, added help.

--- stegaModule/gui.py
+++ stegaModule/gui.py
@@ -3,8 +3,8 @@ from tkinter import ttk
 from tkinter import filedialog
 from pathlib import Path
 import os
-from stegaModule import characterEncrypt
-from stegaModule import characterDecrypt
+import characterEncrypt
+import characterDecrypt
 
 
 #testing commit
@@ -16,16 +16,14 @@ def change_lay(l1, l2, l3, e1, e2, e3, r4, numb="3"):
         e3.grid(row=2,column=1, columnspan=2)
         l2.grid_forget()
         e2.grid_forget()
-
         r4.configure(text="Encrypt Message", font = ('Arial',12), command=lambda: runSt(L1, L2, L3, E1, E2, E3, R4, 1))
     elif numb == 2:
-        l2.grid(row=1, rowspan=1)
-        e2.grid(row=1, column=1, columnspan=2, rowspan=1)
+        l2.grid(row=1, rowspan=2)
+        e2.grid(row=1, column=1, columnspan=2, rowspan=2)
         l3.grid_forget()
         e3.grid_forget()
         l1.grid_forget()
         e1.grid_forget()
-
         r4.configure(text="Decrypt Message", font = ('Arial',12), command=lambda: runSt(L1, L2, L3, E1, E2, E3, R4, 2))
     else:
         print("erorr")
@@ -35,6 +33,12 @@ def change_lay(l1, l2, l3, e1, e2, e3, r4, numb="3"):
 def selectF(root):
     global filename
     filename = filedialog.askopenfilename(initialdir="/", title="Select Image file", filetypes=(("png files", "*.png"), ("all files", "*.*")))
+    R3.grid(row=4,column=0,columnspan=3)
+    L5.grid(row=4,column=2,columnspan=1)
+    if(filename!=""):
+        L5.configure(text="✔")
+    else:
+        L5.configure(text="✘")
     
 
 def runSt(l1, l2, l3, e1, e2, e3, r4, numb="3"):
@@ -58,10 +62,13 @@ def runSt(l1, l2, l3, e1, e2, e3, r4, numb="3"):
             print("Error")
         
 def openHelp():
-    help = Tk()
-    help.title = "Help Menu"
-    HL = Label(help, text="Help Info will Go Here")
-    HL.grid(row=0)
+    try:
+        os.startfile("Help.pdf")
+    except:
+        help = Tk()
+        help.title = "Help Menu"
+        HL = Label(help, text="Help PDF is missing! Please veiw help on our website!", font = ('Arial',12))
+        HL.grid(row=0)
 
 def openFAQ():
     FAQ = Tk()
@@ -84,13 +91,15 @@ def checkErrors(l1, l2, l3, e1, e2, e3, r4, numb):
         if(numb == 1 and str(e3.get()) == ""):
             x=True
             message = "Text Field Must Not Be Empty"
-        if(filename == "No File Selected"):
+        if(filename == "No File Selected" or filename == ""):
             x=True
             message = "File Must Be Selected"
         if(x):
             erwin = Tk()
             erwin.title = "Error"
+            message="Notice: "+message
             EWL = Label(erwin, text=message, font = ('Arial',12))
+            EWL.configure(bg = 'White', fg = 'Red')
             EWL.grid(row=0)
             return False
         else:
@@ -130,6 +139,7 @@ L3 = Label(root, text="Text to be Encrypted:", font = ('Arial',12))
 E3 = Entry(root, bd=1)
 
 R3 = Button(root, text="Select Image",  font = ('Arial',12), command=lambda: selectF(root))
+L5 = Label(root, text="", font = ('Arial',12))
 
 
 R4 = Button(root, text="Encrypt Message",  font = ('Arial',12), command=lambda: runSt(L1, L2, L3, E1, E2, E3, R4, 1))
