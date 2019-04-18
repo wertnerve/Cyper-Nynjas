28b728214e4c78f524c5e7a400cb7c692622d44d
Evan Solosky <soloskye538@strose.edu>
2019-04-18 14:29:34 -0400

    Client-side code for messaging system
    Signed-off-by: Evan Solosky <soloskye538@strose.edu>

--- stegaModule/client.py
+++ stegaModule/client.py
@@ -2,23 +2,43 @@ import socket                   # Import socket module
 import sys
 from PIL import Image
 from io import StringIO, BytesIO
-import tkinter
+from tkinter import *
 from tkinter import filedialog
 
+#Connect to a server, currently hosted on localhost
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             # Create a socket object
 host = socket.gethostbyname('localhost')     # Get local machine name
 port = 9999                    # Reserve a port for your service.
-
 s.connect((host, port))
-'''
-filetosend = filedialog.askopenfilename()
+
+#Create basic GUI for easy file input
+window = Tk()
+window.title("Send a File")
+window.geometry('350x200')
+lbl = Label(window, text="File Upload")
+lbl.grid(column=0, row=0)
+txt = Entry(window, width=10)
+txt.grid(column=1, row=0)
+
+#Function that allows user to select a file through Tkinter's GUI
+def clicked():
+    global filetosend
+    f = filedialog.askopenfilename()
+    filetosend = f
+
+btn = Button(window, text="Choose a File", command = clicked)
+btn.grid(column=1, row=0)
+btn2 = Button(window, text="Click to Close", command = window.destroy)
+btn2.grid(column = 1, row = 1)
+window.mainloop()
+
+#Open file as binary data and send it to the server
 with open(filetosend, "rb") as f:
     data = f.read()
-    bytes = bytearray(data)
-    s.send(bytes)
+    s.send(data)
     print('Successfully get the file')
-    '''
-s.send(bytes("hi", 'utf-8'))
+
+#If a message is sent by the server, recieve the tuple; 'msg' is the var of interest here
 msg, ancdata, flags, addr = s.recvmsg(4096)
 print(msg)
 
--- stegaModule/server.py
+++ stegaModule/server.py
@@ -1,22 +1,24 @@
 import socket                   # Import socket module
 from io import StringIO
 from PIL import Image
-import tkinter
-from tkinter import filedialog
 from io import BytesIO
 
+#Create a server based out of port 9999, hosted on localhost
 port = 9999                    # Reserve a port for your service.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             # Create a socket object
 host = socket.gethostbyname('localhost')     # Get local machine name
 s.bind((host, port))            # Bind to the port
-s.listen(1)                     # Now wait for client connection.
+s.listen(3)                     # Now wait for client connection.
 
 print ('Server listening....')
 
+#Continually loop to accept the clients, max of 3
 while True:
     conn, addr = s.accept()
     print('Got connection from', addr)
+    #If message is sent to the server, receive the tuple
     msg, ancdata, flags, addr = s.recvmsg(4096)
+    #Do something with the binary data (still figuring it out)
     #im = Image.open(BytesIO(data))
     conn.send(msg)
 
