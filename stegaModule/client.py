70c6e6d74a69053e67eb1d20427405582a1d5492
Evan Solosky <soloskye538@strose.edu>
2019-04-30 11:13:00 -0400

    Updated client/server stuff

--- stegaModule/client.py
+++ stegaModule/client.py
@@ -1,32 +1,37 @@
 #NEED TO ADD OPTION TO GUI TO CHOOSE WHAT FILE THE IMAGE IS READ INTO
-
-import socket                   # Import socket module
-import sys
-from PIL import Image
-from io import StringIO, BytesIO
+#This is the working program NOT client.py
+import socket
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
-def sendFile(filetosend):
-    with open(filetosend, "rb") as myfile:
+def sendFile(filename):
+    with open(filename, "rb") as myfile:
+        print('file opened')
         data = myfile.read()
-        s.send(data)
-        print('Successfully send the file')
-        clientfile = open('encrypted-image.jpg', 'wb')
+        print('data read')
+    s.send(data)
+    print('Successfully send the file')
+
+def recieveFile():
+    messagebox.showinfo("", "Please create a file in which to store the recieved image.")
+    myfile = filedialog.asksaveasfilename(initialdir="/", title="Select file",
+                                 filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+    window.update()
+    clientfile = open(myfile, 'wb')
     while True:
         try:
             msg, ancdata, flags, addr = s.recvmsg(999999999)
+            print('received data')
             clientfile.write(msg)
-            # conn.send(msg)
         except:
+            clientfile.close()
             print('no more bytes')
-
 
 #Connect to a server, currently hosted on localhost
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             # Create a socket object
 host = socket.gethostbyname('localhost')     # Get local machine name
 port = 60000                   # Reserve a port for your service.
-s.connect(('66.195.11.250', port))
+s.connect(('192.168.43.103', port))
 
 #Create basic GUI for easy file input
 window = Tk()
@@ -41,12 +46,11 @@ txt.grid(column=1, row=0)
 def clicked():
     f = filedialog.askopenfilename()
     sendFile(f)
+    messagebox.showinfo("","File sent!")
+    window.update()
 
-btn = Button(window, text="Choose a File", command = clicked)
+btn = Button(window, text="Send File", command = clicked)
 btn.grid(column=1, row=0)
-btn2 = Button(window, text="Click to Close", command = window.destroy)
+btn2 = Button(window, text="Recieve File", command = recieveFile)
 btn2.grid(column = 1, row = 1)
 window.mainloop()
-
-
-print('connection closed')
--- stegaModule/server.py
+++ stegaModule/server.py
@@ -1,16 +1,12 @@
 import socket                   # Import socket module
 import time
-from io import StringIO
-from PIL import Image
-from io import BytesIO
 
-#Create a server based out of port 9999, hosted on localhost
 port = 60000                 # Reserve a port for your service.
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)             # Create a socket object
 host = socket.gethostbyname('localhost')     # Get local machine name
-s.bind((host, port))            # Bind to the port
+s.bind(('', port))            # Bind to the port
 print ('Server listening....')
-s.listen(1)                     # Now wait for client connection.
+s.listen(2)                     # Now wait for client connection.
 conn, addr = s.accept()
 print('Got connection from', addr)
 
@@ -25,6 +21,7 @@ def handleConn():
             print('Listening for messages')
         except:
             print('no more bytes')
+            return False
 while True:
     handleConn()
 
