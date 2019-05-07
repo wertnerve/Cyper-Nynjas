89a09732252a643849c0baadd8e46142079c2ecd
Theodore Stabile <stabilet327@strose.edu>
2019-05-07 19:18:03 -0400

    Update characterEncrypt.py

--- stegaModule/characterEncrypt.py
+++ stegaModule/characterEncrypt.py
@@ -1,65 +1,80 @@
-from PIL import Image
 try:
-    import stegaModule
+    from PIL import Image
+    from stegaModule import EncryptionUtilities
+    from stegaModule import VigenereCipher
+    from stegaModule import characterDecrypt
+    from stegaModule import decryptTest
+    from stegaModule import gui
+    from stegaModule import time
 except:
-    import VigenereCipher
+    import Image
     import EncryptionUtilities
+    import VigenereCipher
+    import characterDecrypt
+    import decryptTest
+    import gui
+    import time
+#for now this does not do anything
+
 
 def encrypt(image, text, password):
-    #before anything else, throw the plaintext into the Vigenere Cipher!
-    print("plaintext before encryption:")
-    print(text)
-    print("ADDING MESSAGE FLAGS")
-    text = EncryptionUtilities.concatMessageFlags(text)
-    print(text)
-    print("plaintext as char list")
-    # convert ciphertext to list of characters
-    characterList = list(text)
-    print(characterList)
-    print("plaintext AFTER VIGNERE ENCRYPTION:")
-    text = VigenereCipher.applyCipher(text, password)
-    print(text)
-    print("Ciphertext as character list")
-    characterList = list(text)
-
-    print(text)
+    #text before any ecnryption/modification is done
+    untouchedText = text
+    eu = EncryptionUtilities
+    #print(text)
     #size of what we can - size of text - size of characters for flag is the number you pass
     #to garbage gen(x), it will geenrate that many chaarcters
 
     #split it somewhere random, or down the middle
     #now we need to add garbage
 
-    #first, get pixelBuffer by getting last character of password
-    pixelBuffer = password[len(password) - 1]
-    pixelBuffer = int(pixelBuffer)
-    print(pixelBuffer)
-
-
+  
     # include key for message in the message itself during encryption
     # load image
     img = Image.open(image)
     pixels = img.load()
-
+    #first, get pixelBuffer by getting last character of password
+    pixelBuffer = eu.getPixelBuffer(password)
+    pixelBuffer = 2
+    garbageCharNum = (img.size[0]*img.size[1])-len(text)
+    garbageCharNum -= eu.getMessageFlagLength()
+    garbageCharNum = garbageCharNum // pixelBuffer
     print("Image dimensions", img.size)
-    print("Length:", len(characterList))
+    print("Total number of pixels :",img.size[0]*img.size[1])
+    print("Length of plaintext:", len(text))
     print("Space between pixels:",pixelBuffer)
-    print("Total number of pixels needed:",len(characterList)*pixelBuffer)
+    garbageCharNum=100
+    print("Total # of garbage characters==",garbageCharNum)
+#   GARBAGE WILL BE FULLY ADDED LATER, FOR NOW GABRAGE IS CAPPED AT 1000
+    print("plaintext:",text)
+    text = eu.concatGarbageAndFlags(text,eu.garbageGen(garbageCharNum))
+    print("Length of plaintext after adding flags and garbage:",len(text))
+    print("Total number of pixels needed:",len(text)*pixelBuffer)
 
-    lengthOfText = len(characterList)
-    lengthOfText = int(lengthOfText)
+    lengthOfText = len(text)
     #check is there's enough pixels for the text to be encrypted, first row is reserved for pixelbuffer
     if lengthOfText*pixelBuffer > img.size[0]*img.size[1] :
         print("THERE ARE NOT ENOUGH PIXELS AVAILABLE FOR THIS TEXT")
         return 0
     else :
-        print("REJOICE! THERE ARE ENOUGH PIXELS")
+        print("REJOICE! THERE ARE ENOUGH PIXELS")        
+         #before anything else, throw the plaintext into the Vigenere Cipher!
+        print("plaintext as char list")
+        # convert ciphertext to list of characters
+        characterList = list(text)
+        print(characterList)
 
+        print("plaintext AFTER VIGNERE ENCRYPTION:")
+        text = VigenereCipher.applyCipher(text, password)
+        print(text)
+        print("Ciphertext as character list")
+        characterList = list(text)
+    """
     # store the length of the text with two pixels
     #the first pixel copies the value of the second
     #then the r value of that pixels rbg value becomes r-length
     #do this under the abs() function in case length > r
     #during decrpyt, take the difference in r of both pixels
-    print("Value of pixel before storing length:", pixels[0, 1])
     pixels[0,0] = pixels [1,0]
     placeHolderRGB = list(pixels[1,0])
     r = placeHolderRGB[0]  # original value
@@ -67,52 +82,84 @@ def encrypt(image, text, password):
     b = placeHolderRGB[2]
     r = abs(r-len(characterList))
     pixels[0,0] = (r,g,b)
-#tst
-    print("Value of pixel after storing length:",pixels[1,0])
-
+    
+    """
     # pixel x/y, start off at the second row, first column
-    pX = 1 #start at one since 0 needs to be the placeholder(0 is the first pixel in the row, 1 is the second)
-    pY = 1 #start at second row just to avoid overriding the length and to keep the encrypted pixel bitmap consistent
+    pX = 0 #start at one since 0 needs to be the placeholder(0 is the first pixel in the row, 1 is the second)
+    pY = 0 #start at second row just to avoid overriding the length and to keep the encrypted pixel bitmap consistent
 
     # pixel selection process relies on pixelbuffer and the left adjacent pixel to each chosen pixel
     # pixellBuffer = passed in variable, last character of password, which is between 0-9
     # store characters on each row, with pixelBuffer lenght inbetween
+    curr = 0
     for char in characterList:
+       # print()
+     curr+=1
+     #print(pX,pY)
         # collect color value of current pixel, cast to list
-        rgbValue = list(pixels[pX, pY])
+     rgbValue = list(pixels[pX, pY])
 
-        r = rgbValue[0]  # original value
-        g = rgbValue[1]
-        b = rgbValue[2]
-        print("H")
-        placeHolder = list(pixels[pX - 1, pY])  # r value from adjacent pixele
-        placeHolder = placeHolder[0]
-        print(placeHolder)
-        print(char)
-        print("Storing",char, "ASCII value:",chr(char))
-        r = placeHolder - char
+     r = rgbValue[0]  # original value
+     g = rgbValue[1]
+     b = rgbValue[2]
+     placeHolder = list(pixels[pX - 1, pY])  # r value from adjacent pixele
+     placeHolder = placeHolder[0]
+      #  print(placeHolder)
+       # print(char)
+       # print("Storing",char, "ASCII value:",chr(char))
+     r = placeHolder - char
      #   r = placeHolder - ord(char)  # new value(adjacent pixel's value - char's ASCII value
-
-        pixels[pX, pY] = (r, g, b)
-        pixelXY = '(' + str(pX) + ',' + str(pY) + ')'
-        print("Current pixel location (x,y) is", pixelXY)
-        print("RGB value before adding", char, "=", rgbValue)
-        print("RGB value after adding", char, "=", pixels[pX, pY], '\n')
-
+    
+     pixels[pX, pY] = (r, g, b)
+     pixelXY = '(' + str(pX) + ',' + str(pY) + ')'
+     """
+     if curr < 10:
+      print("Current pixel location (x,y) is", pixelXY)
+      print("RGB value before adding", char, "=", rgbValue)
+      print("RGB value after adding", char, "=", pixels[pX, pY], '\n')
+     """
         # move to the next pixel in column(AKA x), if all pixels in the row have been reached, jump to next row
-        try:
-            pX += pixelBuffer
-            print(pixels[pX,pY])
-        except:
-            print("jumping to next row")
-            pX = 1
-            pY += 1
+
+    # try:
+     #pX += pixelBuffer
+     pX+=2
+     if pX >= img.size[0]:
+        print("jumping to next row")
+        pX = 1
+        pY += 1
+           # print(pixels[pX,pY])
+    """
+     except IndexError:
+            
+     """
 
     # after all ciphertext characters are encrpyted, show original and encrypted image!
     #img.show()
     print(image)
     filename = EncryptionUtilities.trimFilename(image)
     filename+="encryptedImage.png"
+   # filename = "encryptedImage.png"
     img.save(filename)
     eImage = Image.open(filename)
-    #eImage.show()
+    #test if decryption works
+    print()
+    print("Message has been hidden within the image!")
+    print("Testing decrypt of text")
+    plaintextTest = decryptTest.decrypt(filename,password)
+    print()
+    print("retrieved following text:",plaintextTest)
+    if plaintextTest == untouchedText :
+        print()
+        message = "Encryption Succesfull! \nYour encrypted image is saved here:",filename,"\nOpening Encrypted Image in different window:"
+        gui.printAlert("Success!",message)
+       # sleepTime=2
+    else :
+        print()
+        message = "Your encrypted image is saved here:",filename,"\nYour image contains some inconsistent pixels. \nDecrypt may not retrieve entire original text. \nSuggestion: Try using a different Image / password \nOpening image in new window..." 
+        gui.printAlert("Pixel RGB Error.",message)
+       # sleepTime=4
+    print()
+    #time.sleep(sleepTime)
+    eImage.show()
+    
+gui.main()
